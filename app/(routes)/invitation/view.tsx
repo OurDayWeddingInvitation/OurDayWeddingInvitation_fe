512772fe69132e1e6f62f58ae9db4752f116b9c1
KodawonKK <rhekdnjs0169@gmail.com>
2025-11-11 15:23:52 +0900

    feat: header 컴포넌트 invitation/view에 추가

--- app/(routes)/invitation/view.tsx
+++ app/(routes)/invitation/view.tsx
@@ -1,11 +1,15 @@
+import Header from "@/app/components/Header";
 import Form from "./components/form/Form";
 import Preview from "./components/preview/Preview";
 
 export default function InvitationView() {
   return (
-    <div className="max-w-[1200px] py-27 flex mx-auto justify-between">
-      <Preview />
-      <Form />
-    </div>
+    <>
+      <Header showButton={true} showSaveText={true} showTitle={true} />
+      <div className="max-w-[1200px] py-27 flex mx-auto justify-between">
+        <Preview />
+        <Form />
+      </div>
+    </>
   );
 }
