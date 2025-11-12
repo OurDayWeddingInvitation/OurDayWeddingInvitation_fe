6b9e14d063874bd9ae7ed36031cadcc0b763e1c5
chano6 <cocosh6@gmail.com>
2025-11-13 00:35:10 +0900

    feat: 대시보드 청첩장 추가 퍼블리싱

--- app/(routes)/dashboard/view.tsx
+++ app/(routes)/dashboard/view.tsx
@@ -1,8 +1,14 @@
+import Header from "@/app/components/Header";
+import Card from "./components/card/Card";
+
 export default function DashboardView() {
   //page쪽에서 받은 데이터로 zustand세팅 쫙해야함
   return (
-    <div className="flex flex-row items-start max-w-[1200px] mx-auto justify-center ">
-      Dashboard
-    </div>
+    <>
+      <Header />
+      <div className="flex flex-row max-w-[1200px] mx-auto h-dvh justify-center items-center pt-17.5 select-none">
+        <Card />
+      </div>
+    </>
   );
 }
