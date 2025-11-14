a3a73c679da904bcfed241acaab45579e18b101b
KodawonKK <rhekdnjs0169@gmail.com>
2025-11-14 17:46:44 +0900

    feat: 이미지 추가 동작 추가

--- app/(routes)/invitation/components/accordion/section/MainImageSection.tsx
+++ app/(routes)/invitation/components/accordion/section/MainImageSection.tsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageAddBtnIcon from "@/app/assets/images/image-add-btn.svg";
 import Image from "next/image";
 import CheckButton from "@/app/components/CheckButton";
 
 const MainImageSection = () => {
-  const handleImageUpload = () => {};
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    const imageUrl = URL.createObjectURL(file);
+    setPreviewImage(imageUrl);
+  };
   return (
     <div>
       <h3 className="text-[15px] py-3.5">대문 사진</h3>
-      <div className="">
-        <input type="file" id="openingImage" accept="image/*" onChange={handleImageUpload} />
+      <div className="flex gap-2.5">
+        <input type="file" id="openingImage" accept="image/*" onChange={handleImageUpload} className="hidden" />
         <label htmlFor="openingImage">
           <Image src={ImageAddBtnIcon} alt="이미지추가버튼" className="cursor-pointer" />
         </label>
+        {previewImage && (
+          <div className="w-[124px] h-[124px] overflow-hidden ">
+            <img src={previewImage} alt="대문사진" className="w-full h-full object-cover" />
+          </div>
+        )}
       </div>
 
       <h3 className="text-[15px] py-4">대문 스타일</h3>
