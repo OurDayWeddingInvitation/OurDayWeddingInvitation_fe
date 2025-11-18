33a908a4c5d790462d87801a5986cff45029e393
KodawonKK <rhekdnjs0169@gmail.com>
2025-11-18 15:16:53 +0900

    feat : 드래그 안내 문구 추가

--- app/(routes)/invitation/view.tsx
+++ app/(routes)/invitation/view.tsx
@@ -6,9 +6,14 @@ export default function InvitationView() {
   return (
     <>
       <Header showButton={true} showSaveText={true} showTitle={true} />
-      <div className="max-w-[1200px] py-27 flex mx-auto justify-between">
+      <div className="max-w-[1200px] py-27 flex mx-auto">
         <Preview />
-        <Form />
+        <div className="flex-1">
+          <div className="text-[#817E7C] px-4 text-[14px] ">
+            •<span className="bg-[#FFFFFF] rounded-[5px] p-0.5">⠿</span>모양이 있는 메뉴는 드래그하여 순서를 변경할 수 있습니다.
+          </div>
+          <Form />
+        </div>
       </div>
     </>
   );
