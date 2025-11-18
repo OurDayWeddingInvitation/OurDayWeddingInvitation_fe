2e814fc2a21edb6b92db1654fda227f5319c341c
KodawonKK <rhekdnjs0169@gmail.com>
2025-11-18 18:41:15 +0900

    feat: shareThumnail 페이지 추가 , 예식 기본 정보 내용 추가

--- app/(routes)/invitation/components/accordion/section/WeddingInfoSection.tsx
+++ app/(routes)/invitation/components/accordion/section/WeddingInfoSection.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Check, ChevronDown } from "lucide-react";
 
 const WeddingInfoSection = () => {
+  const [clickIdx, setClickIdx] = useState(0);
+
   const inputStyle = "outline-0 flex-1 border-[#E0E0E0] border placeholder:text-center rounded-sm text-sm py-1.5 px-1";
+  const fieldGroup = "flex flex-col gap-2.5 w-full";
   const fieldStyle = "flex flex-wrap items-center";
   const labelStyle = "w-1/6 min-w-[50px]";
 
@@ -11,8 +14,9 @@ const WeddingInfoSection = () => {
 
   return (
     <div className="flex flex-col gap-9 w-full pt-4">
+      {/* 신랑 / 신부 예식 기본 정보 */}
       {label.map((role, roleIdx) => (
-        <div className="flex flex-col gap-2.5 w-full" key={roleIdx}>
+        <div className={fieldGroup} key={roleIdx}>
           <div className={fieldStyle}>
             <div className={labelStyle}>{role}</div>
             <div className="flex flex-1 gap-2.5 items-center flex-wrap">
@@ -51,6 +55,48 @@ const WeddingInfoSection = () => {
         </div>
       ))}
       <div className="border-t border-[#E0E0E0]"></div>
+      {/* 순서 */}
+      <div className={fieldStyle}>
+        <div className={labelStyle}>순서</div>
+        <div className="font-light">
+          <div className="flex gap-2.5 items-baseline py-[5px]">
+            {label.map((role, roleIdx) => (
+              <button
+                key={roleIdx}
+                className={`${clickIdx === roleIdx ? "bg-[#D2BEA9]" : "bg-[#FFFFFF]"} ${clickIdx === roleIdx ? "text-[#FFFFFF]" : "text-[#9C9C9C]"} ${
+                  clickIdx === roleIdx ? "border-[#D2BEA9]" : "border-[#E0E0E0]"
+                } border px-4.5 py-1.5 max-w-[150px] rounded-sm cursor-pointer w-[150px]`}
+                onClick={() => {
+                  setClickIdx(roleIdx);
+                }}
+              >
+                {role} 이름 먼저
+              </button>
+            ))}
+          </div>
+          <p className="text-[#CACACA] text-[12px] leading-[26px]">청첩장 전체에 신랑 측 정보가 먼저 표기됩니다.</p>
+        </div>
+      </div>
+      <div className="border-t border-[#E0E0E0]"></div>
+      {/* 예식 일자 / 시간*/}
+      <div className={fieldGroup}>
+        <div className={fieldStyle}>
+          <div className={labelStyle}>예식 일자</div>
+        </div>
+        <div className={labelStyle}>예식 시간</div>
+        {/* 예식장 명 / 층과 홀*/}
+      </div>
+      <div className="border-t border-[#E0E0E0]"></div>
+      <div className={fieldGroup}>
+        <div className={fieldStyle}>
+          <div className={labelStyle}>예식장명</div>
+          <input type="text" placeholder="예식장명" className={`${inputStyle} min-w-20 max-w-[230px]`} />
+        </div>
+        <div className={fieldStyle}>
+          <div className={labelStyle}>층과 홀</div>
+          <input type="text" placeholder="층과 홀" className={`${inputStyle} min-w-20 max-w-[230px]`} />
+        </div>
+      </div>
     </div>
   );
 };
