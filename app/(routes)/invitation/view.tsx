490c359dba4a16262b5a4f8a8ce6591b0258f529
KodawonKK <rhekdnjs0169@gmail.com>
2025-11-18 15:06:06 +0900

    refactor : import preview 파일명 변경

--- app/(routes)/invitation/view.tsx
+++ app/(routes)/invitation/view.tsx
@@ -1,6 +1,6 @@
 import Header from "@/app/components/Header";
 import Form from "./components/form/Form";
-import Preview from "./components/preview/preview";
+import Preview from "./components/preview/Preview";
 
 export default function InvitationView() {
   return (
