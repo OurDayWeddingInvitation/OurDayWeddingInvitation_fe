d99c343e07f9457ea1a34def0cf656c3fd9d0d23
KodawonKK <rhekdnjs0169@gmail.com>
2025-11-19 18:45:23 +0900

    feat : 가족 관계 상수 업데이트

--- app/lib/constants/index.ts
+++ app/lib/constants/index.ts
@@ -1,3 +1,4 @@
 export { invitationMenu } from "./invitation-menu";
 export type { InvitationMenuItem } from "./invitation-menu";
 export { sectionComponents } from "./invitaion-content";
+export { familyOptionsMale, familyOptionsFemale } from "./invitation-family";
