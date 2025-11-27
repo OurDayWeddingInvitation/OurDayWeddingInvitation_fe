e3172fb23d6666fdca08514e15e56aaa9651e113
KodawonKK <rhekdnjs0169@gmail.com>
2025-11-27 12:24:26 +0900

    feat: preview 폰트, 배경 변경 되게 적용

--- app/(routes)/invitation/components/preview/preview.tsx
+++ app/(routes)/invitation/components/preview/preview.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useColorFontStore } from "@/app/store/useColorFontStore";
 import { useMessageStore } from "@/app/store/invitationMessageStore";
 import { useCounterStore } from "@/app/store/sectionFirstStore";
 import Image from "next/image";
@@ -8,19 +8,17 @@ import Mockup from "../../../../assets/images/mockup.png";
 const Preview = () => {
   const { title, description } = useCounterStore();
   const { message } = useMessageStore();
+  const { themeColor, fontSize, fontStyle } = useColorFontStore();
 
   return (
     <div className="h-full flex relative">
-      <Image
-        src={Mockup}
-        alt="미리보기목업"
-        className="w-full object-contain"
-      />
+      <Image src={Mockup} alt="미리보기목업" className="w-full object-contain rounded-[75px] overflow-hidden " style={{ background: themeColor }} />
+
       <div className="absolute inset-0 flex items-center justify-center">
         <p>{title}</p>
         <p>{description}</p>
 
-        <div dangerouslySetInnerHTML={{ __html: message }} />
+        <div dangerouslySetInnerHTML={{ __html: message }} style={{ fontSize: fontSize, fontFamily: fontStyle }} />
       </div>
     </div>
   );
