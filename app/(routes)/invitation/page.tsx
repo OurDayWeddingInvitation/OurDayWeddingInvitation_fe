e0aa0b2e1ce28df956e4833a10e3da748dd85b6b
chano6 <cocosh6@gmail.com>
2025-12-08 23:00:46 +0900

    feat: 이미지 조회 api 추가중

--- app/(routes)/invitation/page.tsx
+++ app/(routes)/invitation/page.tsx
@@ -1,5 +1,11 @@
+import { fetchApi, Method } from "@/app/lib/fetches/server";
 import InvitationView from "./view";
 
-export default function Page() {
+export default async function Page() {
+  const mediaRes = await fetchApi({
+    endPoint: "/weddings/1/media/edit",
+    method: Method.GET,
+  });
+
   return <InvitationView />;
 }
--- app/types/media.ts
+++ app/types/media.ts
@@ -1,3 +1,16 @@
+export interface ImageBase {
+  weddingId: number;
+  mediaId: number;
+  imageType: string;
+  displayOrder: number;
+  originalUrl: string;
+  editedUrl: string;
+  fileExtension: string;
+  fileSize: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface ImageUploadParams {
   weddingId: number;
   file: File;
