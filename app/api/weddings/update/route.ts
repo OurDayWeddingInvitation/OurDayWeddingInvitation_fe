5728c7409e5571beab699b7523367f5951bb42d6
KodawonKK <ekdnjs0169@naver.com>
2025-12-30 16:13:40 +0900

    chore : 로그 삭제

--- app/api/weddings/update/route.ts
+++ app/api/weddings/update/route.ts
@@ -5,7 +5,6 @@ export async function PATCH(req: NextRequest) {
   //고정
   const apiDomain = process.env.API_DOMAIN;
   const token = await getToken(req);
-  console.log(token);
 
   req.headers.set("Authorization", `Bearer ${token}`);
 
@@ -14,23 +13,28 @@ export async function PATCH(req: NextRequest) {
   console.log(updated);
 
   try {
-    const data = await fetch(`${apiDomain}/weddings/${weddingId}/sections/${sectionId}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: req.headers.get("authorization") ?? ""
-      },
-      body: JSON.stringify(updated),
-      cache: "no-store"
-    });
+    const data = await fetch(
+      `${apiDomain}/weddings/${weddingId}/sections/${sectionId}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: req.headers.get("authorization") ?? "",
+        },
+        body: JSON.stringify(updated),
+        cache: "no-store",
+      }
+    );
     const json = await data.json();
-    console.log(json);
 
     return NextResponse.json(data, { status: 200 });
   } catch (e) {
     const message = e instanceof Error ? e.message : String(e);
     console.error(message);
 
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
