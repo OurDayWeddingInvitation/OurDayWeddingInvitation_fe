00a3685b8b91ac2795ce9a9bd6fa9aeeaa0c463c
chano6 <cocosh6@gmail.com>
2026-01-04 23:40:56 +0900

    fix: 메인사진 store 타입 오류 수정

--- app/store/useMainImageStore.ts
+++ app/store/useMainImageStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { ImageDetailItem } from "../lib/fetches/invitation/type";
+import { ImageDetailItem } from "../lib/fetches/media/type";
 
 type MainImageProp = {
   mainImageInfo: ImageDetailItem | null;
