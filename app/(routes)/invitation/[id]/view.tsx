1d281d0fc9c835c041cbb2cf2f4902631c88b37e
chano6 <cocosh6@gmail.com>
2026-01-15 21:51:34 +0900

    chore: 부모님 소개 섹션 store 저장 수정

--- app/(routes)/invitation/[id]/view.tsx
+++ app/(routes)/invitation/[id]/view.tsx
@@ -114,8 +114,8 @@ export default function InvitationView({
       }
       // 부모님 이미지
       setParentsImageInfo({
-        groomParentsImage,
-        brideParentsImage,
+        groomParentsImage: groomParentsImage,
+        brideParentsImage: brideParentsImage,
       });
     }
   }, [imageDetail]);
