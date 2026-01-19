93d75a084330313e8c94026127e7499d5540d8b0
chano6 <cocosh6@gmail.com>
2026-01-19 22:52:22 +0900

    fix: 컴플릭트 오류 수정

--- app/api/weddings/route.ts
+++ app/api/weddings/route.ts
@@ -46,11 +46,7 @@ export async function POST(req: NextRequest) {
       cache: "no-store",
     });
 
-<<<<<<< Updated upstream
     const json = await data.json();
-=======
-    const json = data.json();
->>>>>>> Stashed changes
 
     return NextResponse.json(json, { status: 200 });
   } catch (e) {
