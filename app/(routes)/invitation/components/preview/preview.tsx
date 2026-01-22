6775cd0eb02b69adb50cb12ce58fd19b5ba10f8d
KodawonKK <ekdnjs0169@naver.com>
2026-01-22 15:59:43 +0900

    fix : 확대 방지 기능 수정

--- app/(routes)/invitation/components/preview/preview.tsx
+++ app/(routes)/invitation/components/preview/preview.tsx
@@ -9,6 +9,7 @@ import { useEffect } from "react";
 import { useLoadingScreenStore } from "@/app/store/useLoadingScreenStore";
 import IntroLoader1 from "./introLoader/IntroLoader1";
 import IntroLoader2 from "./introLoader/IntroLoader2";
+import Head from "next/head";
 
 const Preview = ({ isLink = false }: { isLink?: boolean }) => {
   // const { invitationTitle, invitationMessage } = useMessageStore();
@@ -24,29 +25,45 @@ const Preview = ({ isLink = false }: { isLink?: boolean }) => {
     window.scrollTo(0, 0);
   }, []);
 
-  useEffect(() => {
-    const meta = document.querySelector(
-      'meta[name="viewport"]',
-    ) as HTMLMetaElement;
-
-    if (!meta) return;
-
-    if (zoomPrevent) {
-      meta.setAttribute(
-        "content",
-        "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
-      );
-    } else {
-      meta.setAttribute("content", "width=device-width, initial-scale=1");
-    }
-    console.log(meta);
-  }, [zoomPrevent]);
-
-  return !isLink ? (
-    <div>
-      <div className="relative h-[720px] overflow-scroll [&::-webkit-scrollbar]:hidden z-99">
-        {/* <Image src={Mockup} alt="미리보기목업" className="absolute left-0 top-0 w-full overflow-hidden border" /> */}
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content={
+            zoomPrevent
+              ? "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+              : "width=device-width, initial-scale=1"
+          }
+        />
+      </Head>
+      {!isLink ? (
+        <div>
+          <div className="relative h-[720px] overflow-scroll [&::-webkit-scrollbar]:hidden z-99">
+            {/* <Image src={Mockup} alt="미리보기목업" className="absolute left-0 top-0 w-full overflow-hidden border" /> */}
+            <div
+              style={{
+                scrollbarWidth: "none",
+                msOverflowStyle: "none",
+                backgroundColor: themeFont?.backgroundColor ?? "",
+                fontSize: themeFont?.fontSize ?? 14,
+                fontFamily: fontFamily ?? "",
+              }}
+            >
+              {(menuSetting || [])
+                .filter((item) => item.isVisible)
+                .sort((a, b) => a.displayOrder - b.displayOrder)
+                .map((item) => {
+                  const Component = previewComponents[item.sectionKey];
+                  if (!Component) return null;
+                  return <Component key={item.sectionKey} />;
+                })}
+            </div>
+          </div>
+        </div>
+      ) : (
         <div
+          className="max-w-[480px] w-full"
           style={{
             scrollbarWidth: "none",
             msOverflowStyle: "none",
@@ -55,42 +72,22 @@ const Preview = ({ isLink = false }: { isLink?: boolean }) => {
             fontFamily: fontFamily ?? "",
           }}
         >
+          {introLoader?.design === "1" ? <IntroLoader1 /> : <IntroLoader2 />}
           {(menuSetting || [])
             .filter((item) => item.isVisible)
             .sort((a, b) => a.displayOrder - b.displayOrder)
             .map((item) => {
               const Component = previewComponents[item.sectionKey];
               if (!Component) return null;
-              return <Component key={item.sectionKey} />;
+              return (
+                <FadeInSection key={item.sectionKey}>
+                  <Component />
+                </FadeInSection>
+              );
             })}
         </div>
-      </div>
-    </div>
-  ) : (
-    <div
-      className="max-w-[480px] w-full"
-      style={{
-        scrollbarWidth: "none",
-        msOverflowStyle: "none",
-        backgroundColor: themeFont?.backgroundColor ?? "",
-        fontSize: themeFont?.fontSize ?? 14,
-        fontFamily: fontFamily ?? "",
-      }}
-    >
-      {introLoader?.design === "1" ? <IntroLoader1 /> : <IntroLoader2 />}
-      {(menuSetting || [])
-        .filter((item) => item.isVisible)
-        .sort((a, b) => a.displayOrder - b.displayOrder)
-        .map((item) => {
-          const Component = previewComponents[item.sectionKey];
-          if (!Component) return null;
-          return (
-            <FadeInSection key={item.sectionKey}>
-              <Component />
-            </FadeInSection>
-          );
-        })}
-    </div>
+      )}
+    </>
   );
 };
 
