3a58e00b92f86cf157a0ca52a3af80132aa38149
chano6 <cocosh6@gmail.com>
2026-02-01 16:29:02 +0900

    feat: 계좌정보가 입력되지 않은 경우 미리보기에서 계좌정보 안보이도록 수정

--- app/(routes)/invitation/components/preview/AccountInfo.tsx
+++ app/(routes)/invitation/components/preview/AccountInfo.tsx
@@ -133,28 +133,39 @@ const AccountInfo = ({ isLink }: { isLink: boolean }) => {
                 </AccordionTrigger>
 
                 <AccordionContent className="py-4 flex flex-col gap-2">
-                  {group.accounts.map((acc, idx) => (
-                    <React.Fragment key={idx}>
-                      <div className="flex gap-1">
-                        <h3>{acc.label}</h3>
-                        <span>{acc.holder}</span>
-                      </div>
+                  {group.accounts
+                    .filter(
+                      (acc) =>
+                        acc.label === "신랑님" ||
+                        acc.label === "신부님" ||
+                        acc.holder ||
+                        acc.bank ||
+                        acc.number
+                    )
+                    .map((acc, idx, filteredArr) => (
+                      <React.Fragment key={idx}>
+                        <div className="flex gap-1">
+                          <h3>{acc.label}</h3>
+                          <span>{acc.holder}</span>
+                        </div>
 
-                      <div className="flex justify-between items-center">
-                        <p>{`${acc.bank ?? ""} ${acc.number ?? ""}`}</p>
-                        <span
-                          className="p-2 rounded-full shadow-[2px_4px_4px_rgba(0,0,0,0.1)] cursor-pointer"
-                          onClick={() => {
-                            handleCopy(`${acc.bank ?? ""} ${acc.number ?? ""}`);
-                          }}
-                        >
-                          <Copy color="#CACACA" size={16} />
-                        </span>
-                      </div>
+                        <div className="flex justify-between items-center">
+                          <p>{`${acc.bank ?? ""} ${acc.number ?? ""}`}</p>
+                          <span
+                            className="p-2 rounded-full shadow-[2px_4px_4px_rgba(0,0,0,0.1)] cursor-pointer"
+                            onClick={() => {
+                              handleCopy(
+                                `${acc.bank ?? ""} ${acc.number ?? ""}`
+                              );
+                            }}
+                          >
+                            <Copy color="#CACACA" size={16} />
+                          </span>
+                        </div>
 
-                      {idx !== group.accounts.length - 1 && <Divider />}
-                    </React.Fragment>
-                  ))}
+                        {idx !== filteredArr.length - 1 && <Divider />}
+                      </React.Fragment>
+                    ))}
                 </AccordionContent>
               </AccordionItem>
             ))}
